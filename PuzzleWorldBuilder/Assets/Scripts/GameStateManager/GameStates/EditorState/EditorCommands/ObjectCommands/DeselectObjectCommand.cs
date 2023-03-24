4d4318f004c3ec060f7e1f48fc1c649a6631d615
McYvar <yvarftoorop@hotmail.com>
2023-03-24 17:31:26 +0100

    And added it again

---
+++ PuzzleWorldBuilder/Assets/Scripts/GameStateManager/GameStates/EditorState/EditorCommands/ObjectCommands/DeselectObjectCommand.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DeselectObjectCommand : BaseEditorCommand
+{
+    LinkedList<TerrainObject[]> undoLinkedList;
+    Stack<TerrainObject[]> redoStack;
+    List<TerrainObject> preDeselected;
+    
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        undoLinkedList = new LinkedList<TerrainObject[]>();
+        redoStack = new Stack<TerrainObject[]>();
+        preDeselected = new List<TerrainObject>();
+    }
+
+    public override void Execute()
+    {
+        TerrainObject[] terrainObjects = preDeselected.ToArray();
+        foreach (TerrainObject terrainObject in terrainObjects)
+        {
+            terrainObject.OnDeselection();
+        }
+        preDeselected.Clear();
+        redoStack.Clear();
+        undoLinkedList.AddLast(terrainObjects);
+    }
+
+    public override void Undo()
+    {
+        TerrainObject[] terrainObjects = undoLinkedList.Last.Value;
+        undoLinkedList.RemoveLast();
+        foreach (TerrainObject terrainObject in terrainObjects)
+        {
+            terrainObject.OnSelection();
+        }
+        preDeselected.Clear();
+        preDeselected.AddRange(terrainObjects);
+        redoStack.Push(terrainObjects);
+    }
+
+    public override void Redo()
+    {
+        TerrainObject[] terrainObjects = redoStack.Pop();
+        foreach (TerrainObject terrainObject in terrainObjects)
+        {
+            terrainObject.OnDeselection();
+        }
+        preDeselected.Clear();
+        undoLinkedList.AddLast(terrainObjects);
+    }
+
+    public override void ClearFirstUndo()
+    {
+        undoLinkedList.RemoveFirst();
+    }
+
+    public override void ClearRedo()
+    {
+        redoStack.Clear();
+    }
+
+    public void InitializePreDeselected(List<TerrainObject> preDeselected)
+    {
+        this.preDeselected.Clear();
+        this.preDeselected.AddRange(preDeselected);
+    }
+}
